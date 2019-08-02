0f296e8b515978ff7e3ef0ce391ddfeac2ec3c77
Farshad Fahimi <farshadfahimi@Farshads-MBP.fritz.box>
2019-08-02 09:59:52 +0200

    Add logiticregression and update requirements.txt

--- kernel.py
+++ kernel.py
@@ -4,6 +4,7 @@ import pandas as pd
 import warnings
 from sklearn.preprocessing import Imputer
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 
 from sklearn.model_selection import cross_val_score, StratifiedKFold, GridSearchCV
 from sklearn.metrics import accuracy_score
@@ -36,6 +37,7 @@ def main():
 	heros_info["Height"]=imp.fit_transform(heros_info[["Height"]])
 	heros_info["Weight"]=imp.fit_transform(heros_info[["Weight"]])
 
+	# ...............................
 	# Reading powers csv
 	heros_power = pd.read_csv(POWER_PATH)
 
@@ -47,31 +49,42 @@ def main():
 	# index categorical data of hero info
 	heros_info_dummies = pd.get_dummies(heros_info, columns=info_cat_columns)
 	
+	# ..................................
 	# join data files on name column
 	heros = pd.merge(heros_info_dummies, heros_power_dummies, left_on=['name'], right_on=['hero_names'], how='inner')
 
+	# ..................................
 	# Prepare input and output variables X, y
 	X_columns_drop = ['name', 'hero_names', 'Race', 'Human']
 	X, y = heros.drop(X_columns_drop, axis=1), heros['Human']
 
+	# ....................................
 	# Initialize a stratified split of our dataset for the validation process
 	skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-
 	# Initialize the classifier with the default parameters 
 	rfc = RandomForestClassifier(random_state=42, max_depth=8, n_jobs=-1, oob_score=True)
-
 	# Train it on the training set
 	results = cross_val_score(rfc, X, y, cv=skf)
-
 	# Evaluate the accuracy on the test set
 	print("RandomForest CV accuracy score: {:.2f}%".format(results.mean()*100))
-
+	
 	# Calculating confution matrix for randomforest, more description in readme
 	y_pred = cross_val_predict(rfc, X, y, cv=5)
 	conf_mat = confusion_matrix(y, y_pred)
 	print("RandomForest ConfusionMatrix: {}".format(conf_mat))
 
-	
+	# ............................
+	kf = StratifiedKFold(n_splits=5,shuffle=True,random_state=42)
+	# Initialize the classifier with the default parameters 
+	lgr = LogisticRegression(C=2)
+	# Train it on the training set
+	results = cross_val_score(lgr, X, y, cv=skf)
+	# Evaluate the accuracy on the test set
+	print("Logistic Regression CV accuracy score: {:.2f}%".format(results.mean()*100))
+
+	y_pred = cross_val_predict(lgr, X, y, cv=5)
+	conf_mat = confusion_matrix(y, y_pred)
+	print("LogisticRegression ConfusionMatrix: {}".format(conf_mat))
 
 
 if __name__ == '__main__':
